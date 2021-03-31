a652b10199a2e94367946290292d8e92436646c2
Tim <tim.r.burr@gmail.com>
2021-03-31 13:27:50 -0600

    THIS IS SPARTAA

---
+++ server/app.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const app = express();
+const port = 3000;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static(path.resolve('../public')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.status(200).sendFile('index.html', {
+    root: path.resolve('../public')
+})});
+
+// str.charCodeAt(0) -> Unicode value
+// String.fromCharCode() -> back to a char
+function rotate(s) {
+
+    if(s === " ") {
+        return s;
+    }
+    // 'a' = 97
+    var unicode = s.charCodeAt(0);
+    var newLetter = ((unicode - 97) + 13) % 26;
+    return String.fromCharCode(newLetter + 97);
+}
+
+app.get('/encrypt', (request, response)  => {
+    
+    //TODO: var key = request.query.key;
+    var plaintext = request.query.plaintext;
+    var ciphertext = "";
+
+    for(var n=0; n < plaintext.length; n++) {
+        ciphertext += rotate(plaintext[n].toLowerCase())
+    }
+
+    response.status(200).send( ciphertext );
+})
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
